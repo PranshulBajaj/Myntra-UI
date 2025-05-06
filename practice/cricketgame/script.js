1700be2b4e9e6b8ad1dfaa5249ecfb0166a5a9d2
PranshulBajaj <pranshulmsit@gmail.com>
2025-05-07 01:40:13 +0530

    Initial commit - Myntra Clone

--- practice/cricketgame/script.js
+++ practice/cricketgame/script.js
@@ -10,11 +10,21 @@ function getCompChoice(){
     return 'Stump';
   }
 }
-let score={
+
+let score= JSON.parse( localStorage.getItem('score'))||{
   win:0,
   lost:0,
   tie:0,
 };
+
+document.querySelector('#score-card').innerText=`Win : ${score.win} , Lost : ${score.lost} , Tie : ${score['tie']}`;
+
+function reset(){
+  localStorage.clear();
+  score={win:0,lost:0,tie:0};
+  document.querySelector('#score-card').innerText=`Win : ${score.win} , Lost : ${score.lost} , Tie : ${score['tie']}`;
+}
+
 function getResult(userChoice,compChoice){
   if(userChoice==='Bat'&&compChoice=='Ball'){
     score.win++;
@@ -45,9 +55,12 @@ function getResult(userChoice,compChoice){
     return (`It's a Tie.`);
   }
 }
+
 function showResult(userChoice){
   let compChoice = getCompChoice();
-  alert(`Your choice is ${userChoice}. Computer choice is ${compChoice}. 
-  ${getResult(userChoice,compChoice)}
-  Win : ${score.win} , Lost : ${score.lost} , Tie : ${score['tie']}`);
+  document.querySelector('#score-card').innerText=(`Your choice is ${userChoice}.
+    Computer choice is ${compChoice}. 
+    ${getResult(userChoice,compChoice)}
+    Win : ${score.win} , Lost : ${score.lost} , Tie : ${score['tie']}`);
+  localStorage.setItem('score',JSON.stringify(score));
 }
